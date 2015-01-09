5cca6f111648dc2ccf6658b878a300e7a38158e2
Carlos Garnacho <carlosg@gnome.org>
2015-01-09 17:20:22 +0100

    wayland: Add MetaWaylandTablet
    This (very basic at the moment) struct keeps server-side information
    for wl_tablet resources.

--- src/wayland/meta-wayland-types.h
+++ src/wayland/meta-wayland-types.h
@@ -31,6 +31,8 @@ typedef struct _MetaWaylandTouch MetaWaylandTouch;
 typedef struct _MetaWaylandDataSource MetaWaylandDataSource;
 typedef struct _MetaWaylandDataDevice MetaWaylandDataDevice;
 
+typedef struct _MetaWaylandTablet MetaWaylandTablet;
+
 typedef struct _MetaWaylandBuffer MetaWaylandBuffer;
 typedef struct _MetaWaylandRegion MetaWaylandRegion;
 
